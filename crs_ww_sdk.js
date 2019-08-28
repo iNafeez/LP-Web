8c9f98877477cfae5730c7e742c6d31eea13a0c5
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 10:46:25 -0500

    Update crs_ww_sdk.js

--- crs_ww_sdk.js
+++ crs_ww_sdk.js
@@ -1,4 +1,3 @@
-<script>
 	var SDK = lpTag.agentSDK || {};
 $(function() {
     SDK.init({
@@ -12,9 +11,9 @@ function bind() {
     var bindKey = $(".bindInput").val();
     SDK.bind(bindKey, bindSuccess, createCallback('Bind'));
 
+}
+
 function bindSuccess(data){
     $(".bindResults").html(JSON.stringify(data));
     getLogFunction('INFO', 'Bind success!')(data);
 }
-
-</script>
