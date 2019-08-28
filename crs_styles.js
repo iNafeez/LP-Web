d0f8b002739abf7cc514ba79acdaaf1b25910557
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 16:29:25 -0500

    Update crs_styles.js

--- crs_styles.js
+++ crs_styles.js
@@ -1,6 +1,13 @@
-function addStyleString(str) {
-    var node = document.createElement('style');
-    node.innerHTML = str;
-    document.body.appendChild(node);
+function addCss(rule) {
+  let css = document.createElement('style');
+  css.type = 'text/css';
+  if (css.styleSheet) css.styleSheet.cssText = rule; // Support for IE
+  else css.appendChild(document.createTextNode(rule)); // Support for the rest
+  document.getElementsByTagName("head")[0].appendChild(css);
 }
-addStyleString('@keyframes fadein {50%{opacity:0}100%{opacity:1}}');
+
+// CSS rules
+let rule  = '@keyframes fadein {50%{opacity:0}100%{opacity:1}}';
+
+// Load the rules and execute after the DOM loads
+window.onload = function() {addCss(rule)};
