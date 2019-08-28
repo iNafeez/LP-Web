534ae1ad4a51b074d38d5edeb426315542d9235b
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 18:20:57 -0500

    Update crs_styles.js

--- crs_styles.js
+++ crs_styles.js
@@ -1,7 +1,8 @@
- document.addEventListener("DOMContentLoaded", function(event){
-	function ChatFlexLoanBubble() {
+waitForElement("#FlexLoanTopChatBubble").then(function(element) {
+    	function ChatFlexLoanBubble() {
 	  const displayNoneFlexLoanTopChatBubble = document.getElementById("FlexLoanTopChatBubble");
 	  displayNoneFlexLoanTopChatBubble.style.display = displayNoneFlexLoanTopChatBubble.style.display === 'none' ? '' : 'none';
 	}
 	setTimeout("ChatFlexLoanBubble()", 5000); // after 5 secs
+);
 });
