cbea512a33509a4a8b441e0a54cef5f2bf92e21f
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 18:25:18 -0500

    Update crs_styles.js

--- crs_styles.js
+++ crs_styles.js
@@ -1,7 +1,35 @@
+function waitForElement(selector) {
+  return new Promise(function(resolve, reject) {
+    var element = document.querySelector(selector);
+
+    if(element) {
+      resolve(element);
+      return;
+    }
+
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        var nodes = Array.from(mutation.addedNodes);
+        for(var node of nodes) {
+          if(node.matches && node.matches(selector)) {
+            observer.disconnect();
+            resolve(node);
+            return;
+          }
+        };
+      });
+    });
+
+    observer.observe(document.documentElement, { childList: true, subtree: true });
+  });
+}
+
+
 waitForElement("#FlexLoanTopChatBubble").then(function(element) {
     	function ChatFlexLoanBubble() {
 	  const displayNoneFlexLoanTopChatBubble = document.getElementById("FlexLoanTopChatBubble");
 	  displayNoneFlexLoanTopChatBubble.style.display = displayNoneFlexLoanTopChatBubble.style.display === 'none' ? '' : 'none';
 	}
 	setTimeout("ChatFlexLoanBubble()", 5000); // after 5 secs
+);
 });
