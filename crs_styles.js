13dab01b11e4cb7f8cc3140ad635201af2b00053
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 17:17:21 -0500

    Update crs_styles.js

--- crs_styles.js
+++ crs_styles.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
 var displayNoneFlexLoanTopChatBubble = document.getElementById('FlexLoanTopChatBubble').style;
 displayNoneFlexLoanTopChatBubble.opacity = 1;
 (function fade(){(displayNoneFlexLoanTopChatBubble.opacity-=.1)<0?displayNoneFlexLoanTopChatBubble.display="none":setTimeout(fade,40)})();
+});
