c0404e23ef06b4267937e75f5c9e9eea9007f238
iNafeez <50920183+iNafeez@users.noreply.github.com>
2019-08-28 16:36:09 -0500

    Update crs_styles.js

--- crs_styles.js
+++ crs_styles.js
@@ -1,13 +1,4 @@
-function addCss(rule) {
-  let css = document.createElement('style');
-  css.type = 'text/css';
-  if (css.styleSheet) css.styleSheet.cssText = rule; // Support for IE
-  else css.appendChild(document.createTextNode(rule)); // Support for the rest
-  document.getElementsByTagName("head")[0].appendChild(css);
+function ChatFlexLoanBubble() {
+  document.getElementById("chatbubble").style.display = "block";
 }
-
-// CSS rules
-let rule  = '@keyframes fadein {50%{opacity:0}100%{opacity:1}}';
-
-// Load the rules and execute after the DOM loads
-window.onload = function() {addCss(rule)};
+setTimeout("ChatFlexLoanBubble()", 5000); // after 5 secs
